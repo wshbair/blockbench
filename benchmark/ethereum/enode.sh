74dcfc6f5e805455bcba0d61b0459ef2dde172b8
Wazen Shbair <engwazen@gmail.com>
2017-06-18 19:53:08 +0200

    Update enode.sh

--- benchmark/ethereum/enode.sh
+++ benchmark/ethereum/enode.sh
@@ -4,3 +4,5 @@ ETH_DATA=/data/eth
     
 geth --datadir=$ETH_DATA --rpc --rpcaddr 0.0.0.0 --rpcport "8000" --rpccorsdomain "*" --gasprice 0 --networkid 9119 --unlock 0 --password <(echo -n "") js <(echo 'console.log(admin.nodeInfo.enode);') 2>/dev/null |grep enode | perl -pe "s/\[\:\:\]/$ip_addr/g" | perl -pe "s/^/\"/; s/\s*$/\"/;"
 
+#export LC_CTYPE=en_US.UTF-8
+#export LC_ALL=en_US.UTF-8
