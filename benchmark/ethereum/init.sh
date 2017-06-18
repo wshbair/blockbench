8ab81360a4af5bd61a35bd3e0fd8e8269da5b119
Wazen Shbair <engwazen@gmail.com>
2017-06-18 14:35:34 +0200

    Update init.sh

--- benchmark/ethereum/init.sh
+++ benchmark/ethereum/init.sh
@@ -3,5 +3,5 @@
 cd `dirname ${BASH_SOURCE-$0}`
 . env.sh
 
-geth --datadir=$ETH_DATA init $ETH_HOME/CustomGenesis"_"$1".json"
+geth --datadir=$ETH_DATA init $ETH_HOME/"CustomGenesis.json"
 geth --datadir=$ETH_DATA --password <(echo -n "") account new
